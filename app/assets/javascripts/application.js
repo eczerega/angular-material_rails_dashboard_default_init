67ff078484dd308a95cb72a75cfbc30a3bec25db
eczerega <eczerega@uc.cl>
2015-12-23 12:28:31 -0300

    first commit with basic dashboard and none funcionality

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,38 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require angular
+//= require angular-ui-router/src/common.js
+//= require angular-ui-router/src/resolve.js
+//= require angular-ui-router/src/state.js
+//= require angular-ui-router/src/stateDirectives.js
+//= require angular-ui-router/src/stateFilters.js
+//= require angular-ui-router/src/templateFactory.js
+//= require angular-ui-router/src/urlMatcherFactory.js
+//= require angular-ui-router/src/urlRouter.js
+//= require angular-ui-router/src/view.js
+//= require angular-ui-router/src/viewDirective.js
+//= require angular-ui-router/src/viewScroll.js
+//= require angular-material
+//= require angular-animate
+//= require angular-aria
+//= require angular-messages
+//= require angular-cookies
+//= require angular-material-sidenav
+//= require angular-resource
+//= require angular-route
+//= require angular-touch
+//= require angular-vertical-menu/build/angular-vertical-menu.js
+//= require angular-vertical-menu/build/angular-sidebar-menu.js
+//= require_tree .
+
+
